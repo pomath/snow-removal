06b01e6fd3d31c6fd2b5a8d9619fa38aefa5018b
Peter <peter.matheny@gmail.com>
2018-08-30 16:04:25 -0500

    For some reason the .sh script got removed

---
+++ utils/get_data.sh
@@ -0,0 +1,22 @@
+WORK=`pwd`
+mkdir DATA
+cd DATA
+years=("2006" "2016")
+days=("001" "365")
+station="sow4"
+for yr in `seq ${years[0]} ${years[1]}`; do
+    mkdir $yr
+    cd $yr
+    for d in `seq ${days[0]} ${days[1]}`; do
+        d3=`printf "%0.3d" ${d}`
+        if [ ! -e "${station}${d3}0.${yr: 2:2}d.Z" ]; then
+            wget ftp://data-out.unavco.org/pub/rinex/obs/${yr}/${d3}/${station}*d.Z
+        fi
+
+        if [ ! -e "${station}${d3}0.${yr: 2:2}n.Z" ]; then
+            wget ftp://data-out.unavco.org/pub/rinex/nav/${yr}/${d3}/${station}*n.Z
+        fi
+    done
+    cd ${WORK}/DATA/
+done
+
